f916e25df5cda42224ce1df1ce9deb8f06ebf10f
alessandroNessi <alessandronessi@msn.com>
2021-11-09 16:18:00 +0100

    first commit

---
+++ database/migrations/2021_11_09_143106_create_packages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->date('departure_date');
+            $table->date('returning_date');
+            $table->string('departure_place', 50);
+            $table->string('landing_place', 50);
+            $table->string('arriving_place', 50);
+            $table->foreignId('id_hotel');
+            $table->text("description");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
